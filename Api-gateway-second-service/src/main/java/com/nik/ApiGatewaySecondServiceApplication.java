54e34190657f7b627203b0fbed69ae53ee176a93
Niket kumar <niketkumar18@gmail.com>
2022-04-19 20:13:18 +0530

    Spring clooud gateway example

---
+++ Api-gateway-second-service/src/main/java/com/nik/ApiGatewaySecondServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewaySecondServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewaySecondServiceApplication.class, args);
+	}
+
+}
